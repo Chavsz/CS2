00975ca7a5a75c67766d7d04db2c408803383ba8
Chavy Tapuyao <martinchavy383@gmail.com>
2025-10-07 21:45:06 +0800

    fix sidebar

--- src/routeSelect.tsx
+++ src/routeSelect.tsx
@@ -1,20 +1,62 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import * as mdIcons from "react-icons/md";
+
 const RouteSelect = () => {
-  return <div className="space-y-1">
-    <p className="text-[13px] font-extralight text-[#696969] hidden md:block">MENU</p>
-    <Link to="/dashboard">
-      <div className="flex items-center gap-2">
-        <p className="text-[13px] font-extralight text-[#696969]">Total</p>
-      </div>
+  const [selected, setSelected] = useState(window.location.pathname);
+
+  const handleSelect = (to: string) => {
+    setSelected(to);
+  };
+
+  return (
+    <div className="space-y-1">
+      <p className="text-[13px] font-extralight text-[#696969] hidden md:block">
+        MENU
+      </p>
+      <Route
+        to="/dashboard"
+        selected={selected}
+        Icon={mdIcons.MdOutlineDashboard}
+        title="Total"
+        handleSelect={handleSelect}
+      />
+      <Route
+        to="/addRecords"
+        selected={selected}
+        Icon={mdIcons.MdCalendarMonth}
+        title="Add Records"
+        handleSelect={handleSelect}
+      />
+    </div>
+  );
+};
+
+type RouteProps = {
+  to: string;
+  selected: string;
+  Icon: React.ElementType;
+  title: string;
+  handleSelect: (to: string) => void;
+};
+
+const Route = ({ to, selected, Icon, title, handleSelect }: RouteProps) => {
+  const isSelected = selected === to;
+  return (
+    <Link
+      to={to}
+      className={`flex items-center md:justify-start justify-center gap-2 w-full rounded px-2 py-2 md:py-1.5 md:text-sm text-1xl transition-all duration-300 ${
+        isSelected
+          ? "bg-[#ddfad7] text-green-600 shadow"
+          : "hover:bg-[#ddfad7] text-[#696969] shadow-none"
+      }`}
+      onClick={() => handleSelect(to)}
+    >
+      <Icon className={`${isSelected ? "text-green-600" : ""}`} />
+      <p className="text-md font-semibold hidden md:block">{title}</p>
     </Link>
-    <Link to="/addRecords">
-      <div className="flex items-center gap-2">
-        <p className="text-[13px] font-extralight text-[#696969]">Add Records</p>
-      </div>
-    </Link>
-  </div>;
+  );
 };
 
 export default RouteSelect;
--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className="p-4 text-white sticky top-0 bg-[#f0faf3] h-screen">
       <div className="top-4 h-[calc(100vh-32px-50px)]">
-      <h1 className="text-xl md:text-2xl font-bold text-center text-green-600 mb-9">PH Emigrants</h1>
+      <h1 className="text-xl md:text-2xl font-bold text-center text-green-600 mb-9 hidden md:block">PH Emigrants</h1>
         <RouteSelect />
       </div>
     </div>
